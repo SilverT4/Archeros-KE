884d5fe017a67dbe81c6d674ccf6006bf4ccd698
devin <devinpschweiss@gmail.com>
2021-07-11 22:03:49 -0400

    Add debug warning shit and update icons

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = TitleState;
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 120; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
@@ -76,7 +76,11 @@ class Main extends Sprite
 		}
 
 		#if cpp
+		#if debug
+		initialState = DebugWarning;
+		#else
 		initialState = Caching;
+		#end
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#else
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
